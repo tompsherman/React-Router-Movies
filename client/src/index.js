551f44a033129261793e202510f70ab9a602e96c
Tom Sherman <tompsherman@gmail.com>
2020-09-14 12:43:13 -0400

    wrapp app in browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom"
+
 
 import './index.css';
 import App from './App';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
